c360f06c7a041404ead26055eabd192ebb1ee9e5
Sebastian <fernandezbarrera.sebastian@gmail.com>
2026-06-27 18:28:06 -0400

    feat: implementacion de clases de permisos para VehicleLogViwset y MediaViewSet

--- apps/vehicles/vehicle/application/permissions.py
+++ apps/vehicles/vehicle/application/permissions.py
@@ -27,3 +27,55 @@ class VehiclePermission(BasePermission):
             return False
 
         return request.user.has_perm(required_perm)
+
+
+class VehicleLogPermissions(BasePermission):
+    """
+    Permission class centralizada para VehicleLogViewSet
+    """
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        perms_map = {
+            "list": "vehicles.view_vehiclelog",
+            "retrieve": "vehicles.view_vehiclelog",
+            "create": "vehicles.add_vehiclelog",
+            "partial_update": "vehicles.change_vehiclelog",
+            "destroy": "vehicles.delete_vehiclelog",
+            # 🔥 acción custom
+            "update_status": "vehicles.update_vehiclelog_status",
+        }
+
+        required_perm = perms_map.get(view.action)
+
+        if not required_perm:
+            return False
+
+        return request.user.has_perm(required_perm)
+
+
+class MediaPermissions(BasePermission):
+    """
+    Permission class centralizada para VehicleLogViewSet
+    """
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        perms_map = {
+            "list": "vehicles.view_media",
+            "retrieve": "vehicles.view_media",
+            "create": "vehicles.add_media",
+            "partial_update": "vehicles.change_media",
+            "destroy": "vehicles.delete_media",
+        }
+
+        required_perm = perms_map.get(view.action)
+
+        if not required_perm:
+            return False
+
+        return request.user.has_perm(required_perm)
